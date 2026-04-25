0f310f053f6094eff2d4138ce5004c2027d95fa1
Zoya <zoyab@berkeley.edu>
2026-04-25 12:01:00 -0700

    initializations

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/media-representation')
+def media_representation():
+    return render_template('media_representation.html')
+
+@app.route('/media-desensitization')
+def media_desensitization():
+    return render_template('media_desensitization.html')
+
+@app.route('/narratives-left-out')
+def narratives_left_out():
+    return render_template('narratives_left_out.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
